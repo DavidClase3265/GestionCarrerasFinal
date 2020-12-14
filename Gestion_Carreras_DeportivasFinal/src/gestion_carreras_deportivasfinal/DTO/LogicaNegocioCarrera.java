fa579c22fd6efcea5570631eafe04efde8fd1f40
Your Name <you@example.com>
2020-12-14 11:48:43 +0100

    PROYECTO TERMINADO

---
+++ Gestion_Carreras_DeportivasFinal/src/gestion_carreras_deportivasfinal/DTO/LogicaNegocioCarrera.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestion_carreras_deportivasfinal.DTO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author David
+ */
+public class LogicaNegocioCarrera {
+    private static List<Carrera> listaCarrera = new ArrayList<>();
+    
+    public static void aniadirCarrera(Carrera carrera){
+        listaCarrera.add(carrera);
+    }
+    
+    public static void borrarCarrera(int posicion){ 
+        listaCarrera.remove(posicion);
+    }
+    
+     public static void actualizarCarrera(Carrera carrera, int posicion){ 
+        listaCarrera.get(posicion).setNombre(carrera.getNombre());
+        listaCarrera.get(posicion).setFecha(carrera.getFecha());
+        listaCarrera.get(posicion).setLugar(carrera.getLugar());
+        listaCarrera.get(posicion).setNumero_max(carrera.getNumero_max());
+        listaCarrera.get(posicion).setMapaCorredor(carrera.getMapaCorredor());
+    }
+      
+          
+    public static List<Carrera> getListaCarrera(){
+        return listaCarrera;
+    }
+}
+
---
+++ Gestion_Carreras_DeportivasFinal/src/gestion_carreras_deportivasfinal/DTO/LogicaNegocioCorredor.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestion_carreras_deportivasfinal.DTO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author David
+ */
+public class LogicaNegocioCorredor {
+    
+    private static List<Corredor> listaCorredor = new ArrayList<>();
+    
+    public static void aniadirCorredor(Corredor corredor){
+        listaCorredor.add(corredor);
+    }
+    
+    public static void borrarCorredor(int posicion){ 
+        listaCorredor.remove(posicion);
+    }
+    
+     public static void actualizarCorredor(Corredor corredor, int posicion){
+        listaCorredor.get(posicion).setNombre(corredor.getNombre());
+        listaCorredor.get(posicion).setDNI(corredor.getDNI());
+        listaCorredor.get(posicion).setFecha_nac(corredor.getFecha_nac());
+        listaCorredor.get(posicion).setDireccion(corredor.getDireccion());
+        listaCorredor.get(posicion).setTelefono(corredor.getTelefono());
+    }
+    
+    public static List<Corredor> getListaCorredor(){
+        return listaCorredor;
+    }
+    
+}
+
